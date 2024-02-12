19c6690bbe49b5306772cfec81c312ceed607616
Pamela Lima <layanepamela98@gmail.com>
2024-02-11 21:37:58 -0300

    Test: implementação de teste

---
+++ src/errors/error_handler.py
@@ -0,0 +1,17 @@
+from src.views.http_types.http_response import HttpResponse
+from .error_types.http_unprocessable_entity import HttpUnprocessableEntityError
+
+
+def handler_errors(error: Exception) -> HttpResponse:
+
+    # enviar log ou enviar email de notificação (ideias)
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={"errors": [{"title": error.name, "detail": error.message}]},
+        )
+
+    return HttpResponse(
+        status_code=500,
+        body={"errors": [{"title": "Server Error", "detail": str(error)}]},
+    )
